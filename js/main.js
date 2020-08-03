88f4f753c59ca59500375d5ba3899b93f3fb76d3
Sharandeep Ghosh <sharandeep23@yahoo.com>
2020-08-03 12:34:20 +0600

    Done

---
+++ js/main.js
@@ -0,0 +1,20 @@
+const numEl = document.querySelector("#number");
+const resultEl = document.querySelector("#result");
+
+document.querySelector("#submit").addEventListener("click", (e) => {
+    // Do nothing if input is empty
+    if (!numEl.value) return;
+
+    // Preventing the default behaviour of forms
+    e.preventDefault();
+
+    // Getting, Converting, and Writing out the value to resultEl
+    const binaryNum = Number(numEl.value).toString(2);
+    resultEl.textContent = binaryNum;
+
+    // making resultEL visible
+    resultEl.style.visibility = "visible";
+
+    // Reseting form field
+    numEl.value = "";
+});
